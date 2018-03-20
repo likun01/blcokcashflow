fc9f93e1f19aa6a5c329a1d99c7638665f81df30
likun01 <likun01@harvestwm.cn>
2018-03-20 09:56:32 +0800

    添加配置文件。

---
+++ config/celery.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+APP_LOCATION=/home/likun/app/blockcashflow
+cd $APP_LOCATION
+source /home/likun/env/bin/activate
+
+function start(){
+    python manage.py celery worker -B -l info >> $APP_LOCATION/logs/task.log 2>&1 &
+    echo "Start celery services [OK]"
+    ps aux|grep python
+}
+function stop(){
+    killall -ulikun -9 python
+    echo "Stop celery services [OK]"
+    ps aux|grep python
+}
+function restart(){
+    killall -ulikun -9 python
+    python manage.py celery worker -B -l info >> $APP_LOCATION/logs/task.log 2>&1 &
+    echo "Restart celery services [OK]"
+    ps aux|grep python
+}
+case $1 in 
+    start)
+        start
+    ;;
+    stop)
+        stop
+    ;;
+    restart)
+        restart
+    ;;
+    *)
+        echo "Usages: [start|stop|restart]"
+    ;;
+esac
