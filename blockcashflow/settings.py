6ccd6d57b32fd3c73d5691261f4ee9a80ac3f9bb
likun01 <likun01@harvestwm.cn>
2018-04-23 14:54:37 +0800

    修改celery配置。

--- blockcashflow/settings.py
+++ blockcashflow/settings.py
@@ -252,16 +252,17 @@ SUIT_CONFIG = {
          'models': ('auth.group',)},
     )
 }
-
+import djcelery
+djcelery.setup_loader()
 # djcelery+broker配置
 BROKER_URL = 'redis://:redis7890@127.0.0.1:6379/1'
 CELERY_RESULT_BACKEND = 'redis://:redis7890@127.0.0.1:6379/1'
 REDIS_CONNECT_RETRY = True
 CELERY_TASK_RESULT_EXPIRES = 10
-CELERY_ACCEPT_CONTENT = ['msgpack', ]
-import djcelery
-djcelery.setup_loader()
-
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 SOUTH_MIGRATION_MODULES = {
     'djcelery': 'djcelery.south_migrations',
 }
