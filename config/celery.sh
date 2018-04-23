1e39dcddd590a7be2f4b92c8abd1cf67478cef4a
likun01 <likun01@harvestwm.cn>
2018-04-23 09:47:41 +0800

    修改celery配置。

--- config/celery.sh
+++ config/celery.sh
@@ -6,18 +6,18 @@ source /home/likun/env/bin/activate
 function start(){
     python manage.py celery worker -B -l info >> $APP_LOCATION/logs/task.log 2>&1 &
     echo "Start celery services [OK]"
-    ps aux|grep python
+    ps aux|grep celery
 }
 function stop(){
     killall -ulikun -9 python
     echo "Stop celery services [OK]"
-    ps aux|grep python
+    ps aux|grep celery
 }
 function restart(){
     killall -ulikun -9 python
     python manage.py celery worker -B -l info >> $APP_LOCATION/logs/task.log 2>&1 &
     echo "Restart celery services [OK]"
-    ps aux|grep python
+    ps aux|grep celery
 }
 case $1 in 
     start)
